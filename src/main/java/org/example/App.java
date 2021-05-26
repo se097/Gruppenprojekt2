4076fec949dfc62e4d308027ec3a058f1bc778df
Sonja Entina <soja_ok@MBP-von-Sonja.fritz.box>
2021-05-26 16:35:27 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/**
+ * A simple HelloWorld style application.
+ */
+
+public class App {
+
+    /**
+     * Your application's main entry point.
+     *
+     * @param args Yet unused
+     */
+    public static void main( String[] args ) {
+       // Multiline text block requires Java 15+
+
+       System.out.println("""
+         Hi there, let's have
+         fun learning Java!""");
+    }
+}
+
